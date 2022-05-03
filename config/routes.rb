7f9735b48b331b12e65e48689e164bea213933fc
Alexa Martinez <alexamirabelle@gmail.com>
2022-05-03 17:54:15 +0800

    created moods controller & view, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#profile'
+  root to: 'moods#index'
+  get 'profile', to: 'pages#profile', as: :profile
+
+
+  resources :moods, only: [ :index, :show, :audio, :movement, :affirmations, :goodvibes]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
