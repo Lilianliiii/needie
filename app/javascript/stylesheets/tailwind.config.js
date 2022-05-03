33525391ad5f8da2af5ac6ea49464927f2538d91
Lilian Li <lil712@hotmail.com>
2022-05-03 12:51:24 +0800

    tailwind set up and navbar done

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headings: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'lightgreen': '#A2D5AB',
+        'neediegreen': '#39AEA9',
+      },
+      textColor: ['visited'],
+    },
   },
   variants: {
     extend: {},
