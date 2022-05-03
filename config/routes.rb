6dea29bd6269d546b80207c109d5347eeb093a00
Valantina Tsoi <valantina.t@live.com>
2022-05-03 18:04:56 +0800

    survey routes, edit and create only

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   resources :moods, only: [ :index, :show, :audio, :movement, :affirmations, :goodvibes]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :survey, only: [ :new, :create]
 end
