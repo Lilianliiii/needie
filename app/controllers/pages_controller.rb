f6dd11ea383e00340ce63f98bd411f2d871a1dfb
Alexa Martinez <alexamirabelle@gmail.com>
2022-05-03 21:44:46 +0800

    Created audio, movement, affirmations and goodvibes actions in controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: :profile
 
   def profile
+    current_user
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+puts "Cleaning database..."
+Mood.destroy_all
+
 happy = Mood.create(name: 'Happy')
 meh = Mood.create(name: 'Meh')
 annoyed = Mood.create(name: 'Annoyed')
@@ -13,7 +17,7 @@ anxious = Mood.create(name: 'Anxious')
 angry = Mood.create(name: 'Angry')
 sad = Mood.create(name: 'Sad')
 
-
+puts "Created Moods!"
 # reading = Reading.create(text: "this is a reading this is a reading this is a reading this is a reading")
 # therapy = Therapy.new(category: 'Affirmations')
 # therapy.content = reading
