ef92ad00ac08bfd0faa6a387156de0ecdd62877e
Valantina Tsoi <valantina.t@live.com>
2022-05-04 17:46:28 +0800

    survey form fitted with javascript, no iteration

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -21,9 +21,13 @@ class SurveysController < ApplicationController
 
   private
 
-  # def score
-  #   @score =
-  # end
+  def score
+    @score = @question_0 + @question_1 + @question_2 + @question_3 + @question_4 + @question_5 + @question_6
+    + @question_7 + @question_8 + @question_9 + @question_10 + @question_11 + @question_12 + @question_13
+    + @question_14 + @question_15 + @question_16 + @question_17 + @question_18 + @question_20 + @question_21
+    + @question_22 + @question_23 + @question_24
+  end
+    # foreach
 
   def survey_params
     params.require(:survey).permit(:user_id, :score)
