fe764ecb9beda4f7dde624e581cbbc6cfd88d923
Lilian Li <lil712@hotmail.com>
2022-05-04 13:39:41 +0800

    added cloudinary and env gems, added image column to moods model, updated homepage to include moods name and image

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@
 puts "Cleaning database..."
 Mood.destroy_all
 
-happy = Mood.create(name: 'Happy')
-meh = Mood.create(name: 'Meh')
-annoyed = Mood.create(name: 'Annoyed')
-anxious = Mood.create(name: 'Anxious')
-angry = Mood.create(name: 'Angry')
-sad = Mood.create(name: 'Sad')
+happy = Mood.create(name: 'Happy', image: 'Happy_upkqw0')
+meh = Mood.create(name: 'Meh', image: 'Meh_g4xvby')
+annoyed = Mood.create(name: 'Annoyed', image: 'Annoyed_ov3xbn')
+anxious = Mood.create(name: 'Anxious', image: 'Anxious_pwzbfk')
+angry = Mood.create(name: 'Angry', image: 'Angry_hioupy')
+sad = Mood.create(name: 'Sad', image: 'Sad_hmij9i')
 
 puts "Created Moods!"
 # reading = Reading.create(text: "this is a reading this is a reading this is a reading this is a reading")
