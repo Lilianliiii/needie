e86cc3499ae98b86e40256b5684fb06b06b70ad4
Maela Bougha <florianemaela.bougha@essca.eu>
2022-05-06 15:46:36 +0800

    Edited profile page

--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,7 @@ ActiveRecord::Schema.define(version: 2022_05_04_050053) do
   end
 
   create_table "surveys", force: :cascade do |t|
-    t.bigint "user_id"
-    t.integer "score"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "question_0"
