6343cfa25d2cf0f0bb9efb47058181478af54bde
Alexa Martinez <alexamirabelle@gmail.com>
2022-05-11 15:07:04 +0800

    map with point and search bar

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,10 +1,10 @@
 class WalksController < ApplicationController
-
   def index
     @walks = Walk.all
     @markers = @walks.geocoded.map do |walk|
       {
         lat: walk.latitude,
+
         lng: walk.longitude
       }
     end
@@ -12,6 +12,23 @@ class WalksController < ApplicationController
 
   def show
     @walk = Walk.find(params[:id])
+      @markers = [
+      {
+        lat: @walk.latitude,
+        lng: @walk.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { walk: @walk }),
+        image_url: helpers.asset_url("steps.png")
+      }]
   end
+
+  def direction
+
+
+  end
+
+
+
+
+
 
 end
